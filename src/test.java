329b3989af1eba326c664752aa47721b63126796
sanki86 <marisanki@gmail.com>
2020-06-24 23:11:58 -0700

    7th

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ package test;
 
 public class test {
 	public static void main(String[] args) {
-		String abc ="ABCDEFGHIJ";
+		String abc ="ABCDEFGHIJK";
 		
 		StringBuilder build = new StringBuilder();
 		build.append(abc);
