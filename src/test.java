5e0291071c8b7b15b7c41beb7da5f3ae95b919eb
sanki86 <marisanki@gmail.com>
2020-06-24 23:07:27 -0700

    3rd

---
+++ src/test.java
@@ -0,0 +1,13 @@
+package test;
+
+public class test {
+	public static void main(String[] args) {
+		String abc ="ABCDEFG";
+		
+		StringBuilder build = new StringBuilder();
+		build.append(abc);
+		build.reverse();
+		System.out.println("reverse string is "+build);
+		
+	}
+}
